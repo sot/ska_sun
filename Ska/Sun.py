502e0b943dcda3eb8613a7afc857fe171e87ac8b
Tom Aldcroft <taldcroft@gmail.com>
2017-01-03 10:17:39 -0500

    Update version to 3.5

--- Ska/Sun.py
+++ Ska/Sun.py
@@ -8,7 +8,7 @@ from math import cos, sin, acos, atan2, asin, pi, radians, degrees, ceil
 import numpy as np
 import Ska.quatutil
 
-__version__ = '0.5'
+__version__ = '3.5'
 
 def position(time):
     """
