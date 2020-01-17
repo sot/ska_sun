555f91cbdd5acbd07969affb238e6910e55f8b91
Javier Gonzalez <javierggt@yahoo.com>
2020-01-17 17:06:05 -0500

    use __name__ instead of __package__ in ska_helpers.get_version

--- Ska/Sun.py
+++ Ska/Sun.py
@@ -10,7 +10,7 @@ import numpy as np
 import Ska.quatutil
 import ska_helpers
 
-__version__ = ska_helpers.get_version(__package__)
+__version__ = ska_helpers.get_version(__name__)
 
 
 # The position() method is a modification of
