ded06ccb0e7a2cdec7160b15dec18b324fe94584
Javier Gonzalez <javierggt@yahoo.com>
2020-10-16 14:51:06 -0400

    Deploy docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.5.4.dev6+g84b576d',
+    VERSION: '3.6.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
