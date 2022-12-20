75f1b7263d3083ed1c5200125479349d3089347d
Javier Gonzalez <javierggt@yahoo.com>
2022-12-20 10:56:16 -0500

    use duplicate_package_info from ska_helpers

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from setuptools import setup
-from testr.setup_helper import cmdclass, duplicate_package_info
+from testr.setup_helper import cmdclass
+from ska_helpers.setup_helper import duplicate_package_info
 
 name = "ska_sun"
 namespace = "Ska.Sun"
